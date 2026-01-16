bca2c47af90a9600e5972c0a83b78c0a258176f2
Solbreen <rundinosawr@gmail.com>
2026-01-17 00:41:27 +0300

    подправил

--- task3/task.py
+++ task3/task.py
@@ -12,11 +12,61 @@ def process_json_files(filenameA, filenameB) -> tuple:
         print(f"Ошибка: {e}")
         return None, None      
     
-
-
 def main(a: list, b: list):
-    print(f"Исходные данные A: {a}")
-    print(f"Исходные данные B: {b}")
+
+    print("\n")
+
+    order = []
+    for item in a:
+        if isinstance(item, list):
+            order.extend(item)
+        else:
+            order.append(item)
+    
+    dictA = {x: 0 for x in order}
+    dictB = {x: 0 for x in order}
+    for i, val in enumerate(a):
+        if isinstance(val, list):
+            for j in val:
+                dictA[j] = i
+        else:
+            dictA[val] = i
+    for i, val in enumerate(b):
+        if isinstance(val, list):
+            for j in val:
+                dictB[j] = i
+        else:
+            dictB[val] = i
+
+    matrixAB = []
+    for row in order:
+        temprowAB = []
+        for col in order:
+            aij = 0 if dictA[col] < dictA[row] else 1
+            bij = 0 if dictB[col] < dictB[row] else 1
+            temprowAB.append(aij * bij)
+        matrixAB.append(temprowAB)
+
+    matrixABT = list(map(list, zip(*matrixAB)))
+
+    finalMatrix = [[a or b for a, b in zip(row1, row2)] for row1, row2 in zip(matrixAB, matrixABT)]
+
+    result = []
+    for i, row in enumerate(finalMatrix):
+        tempr = [order[i]]
+        yes = []
+        for j, val in enumerate(row):
+            if val == 0:
+                tempr.append(order[j])
+        if len(tempr) > 1:
+            if not any(any(item in sublist for item in tempr) for sublist in result):
+                result.append(tempr)
+
+    return json.dumps(result)
+
+
+def task(a: list, b: list):
+
     print("\n")
 
     order = []
@@ -131,5 +181,7 @@ if __name__ == "__main__":
     ah = [1, [2, 3], 4, [5, 6, 7], 8, 9, 10]
     bh = [3, [1, 4], 2, 6, [5, 7, 8], [9, 10]]
 
-    result = main(ah, bh)
+    result = task(ah, bh)
     print(f"Согласованная кластерная ранжировка: f(A, B) = {result}")
+    result = main(ah, bh)
+    print(f"Ядро противоречий для двух ранжировок: S(A, B) = {result}")
